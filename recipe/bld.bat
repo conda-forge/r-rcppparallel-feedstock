99ccdfb2b5059732df1fa0e6cfa6c20330f15287
Isuru Fernando <isuruf@gmail.com>
2024-07-23 16:15:18 -0500

    Use tbb from conda

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,6 @@
+set "TBB_ROOT=%LIBRARY_PREFIX%"
+set "TBB_LIB=%LIBRARY_LIB%"
+set "TBB_INC=%LIBRARY_INC%"
+
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,9 @@
 
 export DISABLE_AUTOBREW=1
 
+export TBB_ROOT=${PREFIX}
+export TBB_LIB=${PREFIX}/lib
+export TBB_INC=${PREFIX}/include
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
