9f594bb8561117da88e77bd15f8d71665d5056ea
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-08-29 15:31:39 +0200

    fix include

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 export DISABLE_AUTOBREW=1
 
 export TBB_LIB="${PREFIX}/lib"
-export TBB_INC="${PREFIX}/include/tbb"
+export TBB_INC="${PREFIX}/include"
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
