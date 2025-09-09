054139649e0a37e3a3e8c7d40c250b9f998719cb
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-09-09 09:29:22 +0200

    debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,9 @@ export DISABLE_AUTOBREW=1
 export TBB_LIB="${PREFIX}/lib"
 export TBB_INC="${PREFIX}/include"
 
+# >>> begin debug
+echo "echo src/Makevars" >> configure
+# <<< end debug
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
