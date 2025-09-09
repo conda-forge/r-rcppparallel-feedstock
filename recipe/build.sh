98c42e2763e225f418e2dd9e8ce0ffd0565014b9
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-09-09 10:35:57 +0200

    check Makeconfs

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,10 @@ export TBB_INC="${PREFIX}/include"
 
 # >>> begin debug
 echo "cat src/Makevars" >> configure
+echo "BUILD_PREFIX Makeconf"
+cat $BUILD_PREFIX/lib/R/etc/Makeconf
+echo "PREFIX Makeconf"
+cat $PREFIX/lib/R/etc/Makeconf
 # <<< end debug
 
 # shellcheck disable=SC2086
