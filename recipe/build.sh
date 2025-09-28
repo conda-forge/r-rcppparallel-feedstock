779be23baab0d419b5346c6591a71e21037a2f3f
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-28 00:29:30 -0700

    add sed

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 export DISABLE_AUTOBREW=1
 
-sed -i "s?PKG_LIBS = ?PKG_LIBS = -L${PREFIX}/lib/R/lib?" src/Makevars.in
+sed -i "s?PKG_LIBS = ?PKG_LIBS = -L${PREFIX}/lib/R/lib ?" src/Makevars.in
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
